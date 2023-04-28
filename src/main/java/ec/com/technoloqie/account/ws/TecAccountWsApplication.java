ed9e177bb287add07e014d3e974721f479d14def
diego-mac <dijavaji@gmail.com>
2023-04-28 12:34:40 -0500

    primer commit

---
+++ src/main/java/ec/com/technoloqie/account/ws/TecAccountWsApplication.java
@@ -0,0 +1,13 @@
+package ec.com.technoloqie.account.ws;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TecAccountWsApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TecAccountWsApplication.class, args);
+	}
+
+}
